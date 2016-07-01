e43d968210974ceb4ac478c90c6c01bd62bae25c
Scott Davis <sc.ttdav.s@gmail.com>
2016-07-01 10:22:09 -0400

    add get and post

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 }('$', this, function () {
   'use strict';
 
+  var errorReachedMsg = "NjqError: Server reached, but returned an error";
+  var errorNotReachedMsg = "NjqError: Connection error, server not reached";
   var NjqError = function(message) {
   	this.message = message;
   };
@@ -22,20 +24,40 @@
         callback(JSON.parse(request.responseText));
       } else {
         // We reached our target server, but it returned an error
-        throw new NjqError("NjqError: Server reached, but returned an error");
+        throw new NjqError(errorReachedMsg);
       }
     };
 
     request.onerror = function(e) {
-      console.log(e.target.status);
-      console.log(e.target);
-      console.log(e);
       // There was a connection error of some sort
-      throw new NjqError("NjqError: Connection error, server not reached");
+      throw new NjqError(errorNotReachedMsg);
     };
 
     request.send();
   }
+
+  njq.ajax = function(options) {
+    var request = new XMLHttpRequest();
+    request.open(options.type, options.url, true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        options.success(request.responseText);
+      } else {
+        // We reached our target server, but it returned an error
+        options.error(new NjqError(errorReachedMsg));
+      }
+    };
+
+    request.onerror = function() {
+      // There was a connection error of some sort
+      options.error(new NjqError(errorNotReachedMsg));
+    };
+
+    request.send(options.data);
+  };
 
   return njq;
 });
--- test/test.js
+++ test/test.js
@@ -10,6 +10,24 @@ zock
 });
 var Promise = require('es6-promise').Promise; // zock needs this
 var domain = "http://www.fake.com";
+var GET = "GET";
+var POST = "POST";
+
+var setGet = function(domain, path, result) {
+  window.XMLHttpRequest = zock
+    .base(domain)
+    .get(path)
+    .reply(200, result)
+    .XMLHttpRequest
+};
+
+var setPost = function(domain, path, result) {
+  window.XMLHttpRequest = zock
+    .base(domain)
+    .post(path)
+    .reply(200, result)
+    .XMLHttpRequest
+};
 
 describe("Basics", function() {
   it("work", function() {
@@ -19,15 +37,52 @@ describe("Basics", function() {
   it("gets JSON", function(done) {
     var result = { test: true };
     var path = "/some/shit";
-    window.XMLHttpRequest = zock
-      .base(domain)
-      .get(path)
-      .reply(200, result)
-      .XMLHttpRequest
+    setGet(domain, path, result);
 
     njq.getJSON(domain + "/some/shit", function(data) {
       expect(data).to.eql(result);
       done();
     });
+  });
+
+  it("posts data", function(done) {
+    var result = { test: false };
+    var path = "/some/shit";
+    setPost(domain, path, result);
+
+    njq.ajax({
+      type: POST,
+      url: domain + "/some/shit",
+      data: result,
+      success: function(data) {
+        expect(JSON.parse(data)).to.eql(result);
+        done();
+      },
+      error: function() {
+        expect("success").to.not.equal("error");
+        done();
+      }
+    });
+  });
+
+  it("gets data", function(done) {
+    var result = { test: false };
+    var path = "/some/shit";
+    setGet(domain, path, result);
+
+    njq.ajax({
+      type: GET,
+      url: domain + "/some/shit",
+      data: result,
+      success: function(data) {
+        expect(JSON.parse(data)).to.eql(result);
+        done();
+      },
+      error: function() {
+        expect("success").to.not.equal("error");
+        done();
+      }
+    });
+
   });
 });
